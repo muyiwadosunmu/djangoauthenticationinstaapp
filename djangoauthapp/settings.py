2ee91b959a960b2d6e98648c29a0fa48c7bde17f
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-07-27 04:31:40 +0100

    Prepare for deployment

--- djangoauthapp/settings.py
+++ djangoauthapp/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.dj angoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4+=%vnrxiru1hj5tp)l2@a3kmw-0mwlono&via-#)p-z0e1ss*'
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-4+=%vnrxiru1hj5tp)l2@a3kmw-0mwlono&via-#)p-z0e1ss*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
